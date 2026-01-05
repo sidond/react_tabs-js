b4ae49dc0871c77c963b64174ba9d176fb1281ec
Dmytro Petrov <sidodji228007@gmail.com>
2026-01-05 12:21:32 +0200

    some corrections implemented

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,6 @@ export const App = () => {
       <Tabs
         tabs={tabs}
         activeTabId={currentTab.id}
-        currentTab={currentTab}
         onTabSelected={setCurrentTab}
       />
     </div>
--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,53 +1,32 @@
 import cn from 'classnames';
 
-export const Tabs = ({ tabs, activeTabId, currentTab, onTabSelected }) => {
+export const Tabs = ({ tabs, activeTabId, onTabSelected }) => {
   return (
     <div data-cy="TabsComponent">
       <div className="tabs is-boxed">
         <ul>
-          <li
-            className={cn({ 'is-active': activeTabId === tabs[0].id })}
-            data-cy="Tab"
-          >
-            <a
-              href="#tab-1"
-              data-cy="TabLink"
-              onClick={() => onTabSelected(tabs[0])}
-            >
-              Tab 1
-            </a>
-          </li>
-
-          <li
-            className={cn({ 'is-active': activeTabId === tabs[1].id })}
-            data-cy="Tab"
-          >
-            <a
-              href="#tab-2"
-              data-cy="TabLink"
-              onClick={() => onTabSelected(tabs[1])}
-            >
-              Tab 2
-            </a>
-          </li>
-
-          <li
-            className={cn({ 'is-active': activeTabId === tabs[2].id })}
-            data-cy="Tab"
-          >
-            <a
-              href="#tab-3"
-              data-cy="TabLink"
-              onClick={() => onTabSelected(tabs[2])}
-            >
-              Tab 3
-            </a>
-          </li>
+          {tabs.map(tab => {
+            return (
+              <li
+                className={cn({ 'is-active': activeTabId === tab.id })}
+                data-cy="Tab"
+                key={tab.id}
+              >
+                <a
+                  href="#tab-1"
+                  data-cy="TabLink"
+                  onClick={() => onTabSelected(tab.id)}
+                >
+                  {tab.title}
+                </a>
+              </li>
+            );
+          })}
         </ul>
       </div>
 
       <div className="block" data-cy="TabContent">
-        {currentTab.content}
+        {tabs[activeTabId].content}
       </div>
     </div>
   );
