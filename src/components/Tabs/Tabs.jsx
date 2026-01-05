dba7439df403cfe83c8d6443f731078ea8f55093
Dmytro Petrov <sidodji228007@gmail.com>
2026-01-05 14:20:41 +0200

    add correctives

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,6 +1,8 @@
 import cn from 'classnames';
 
 export const Tabs = ({ tabs, activeTabId, onTabSelected }) => {
+  const currentTab = tabs.find(tab => tab.id === activeTabId) || tabs[0];
+
   return (
     <div data-cy="TabsComponent">
       <div className="tabs is-boxed">
@@ -16,7 +18,7 @@ export const Tabs = ({ tabs, activeTabId, onTabSelected }) => {
                   href={`#${tab.id}`}
                   data-cy="TabLink"
                   onClick={
-                    activeTabId === tab.id ? '' : () => onTabSelected(tab.id)
+                    activeTabId === tab.id ? null : () => onTabSelected(tab.id)
                   }
                 >
                   {tab.title}
@@ -28,7 +30,7 @@ export const Tabs = ({ tabs, activeTabId, onTabSelected }) => {
       </div>
 
       <div className="block" data-cy="TabContent">
-        {tabs.find(tab => tab.id === activeTabId).content}
+        {currentTab.content}
       </div>
     </div>
   );
